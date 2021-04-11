f9830a16a92d7c490628ed83fd27890a479fe900
Thomas Aegidiussen <thomasaedk@gmail.com>
2021-04-11 20:30:32 +0200

    enemy and emeny movement

---
+++ Assets/Enemy/EnemyMover.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour {
+
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+
+    // Start is called before the first frame update
+    void Start() {
+        PrintWaypointName();
+    }
+
+    private void PrintWaypointName() {
+        foreach(Waypoint waypoint in path) {
+            Debug.Log(waypoint.name);
+        }
+    }
+}
