f515723d077d2cf58882be5d7d9623d1828db388
Thomas Aegidiussen <thomasaedk@gmail.com>
2021-04-14 21:00:09 +0200

    detect mouse input

--- Assets/Tiles/Waypoint.cs
+++ Assets/Tiles/Waypoint.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Waypoint : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Waypoint : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [SerializeField] bool isPlaceable;
+
+    private void OnMouseDown() {
+        if (isPlaceable) {
+            Debug.Log(transform.name);
+        }
     }
 }
