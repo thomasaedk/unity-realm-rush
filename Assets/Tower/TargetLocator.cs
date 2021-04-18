ee435c07d77da57c72fda38f3a08cf95756645f7
Thomas Aegidiussen <thomasaedk@gmail.com>
2021-04-18 15:08:13 +0200

    towers: target and damage enemies

---
+++ Assets/Tower/TargetLocator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetLocator : MonoBehaviour
+{
+
+    [SerializeField] private Transform weapon;
+    private Transform target;
+    
+    void Start()
+    {
+        target = FindObjectOfType<EnemyMover>().transform;
+    }
+
+    void Update() {
+        AimWeapon();
+    }
+
+    private void AimWeapon()
+    {
+        weapon.LookAt(target);
+    }
+}
