057fa8918cb1fd9a6b962db1c0139d638b17b12f
Thomas Aegidiussen <thomasaedk@gmail.com>
2021-04-18 15:05:45 +0200

    formatting changes

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -2,24 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMover : MonoBehaviour {
+public class EnemyMover : MonoBehaviour 
+{
 
-    [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] [Range(0f, 5f)] float speed = 1f;
+    [SerializeField] private List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] [Range(0f, 5f)] private float speed = 1f;
 
-    void Start() {
+    private void Start() 
+    {
         StartCoroutine(FollowPath());
     }
 
-    private IEnumerator FollowPath() {
-        foreach(Waypoint waypoint in path) {
+    private IEnumerator FollowPath() 
+    {
+        foreach(Waypoint waypoint in path) 
+        {
             Vector3 startPosition = transform.position;
             Vector3 endPosition = waypoint.transform.position;
             float travelPercent = 0f;
 
             transform.LookAt(endPosition);
 
-            while (travelPercent < 1f) {
+            while (travelPercent < 1f) 
+            {
                 travelPercent += Time.deltaTime * speed;
                 transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
                 yield return new WaitForEndOfFrame();
